d01b29517f017e383fefd1f87ade58a2af69e97f
Pavlo Yeremeiew <peremuv3@gmail.com>
2025-02-11 11:27:15 +0200

    fix QuestionEditor

--- MathMasters/Assets/Scripts/Entities/Question.cs
+++ MathMasters/Assets/Scripts/Entities/Question.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MathMasters.Entities
@@ -8,15 +6,28 @@ namespace MathMasters.Entities
     public class Question : ScriptableObject
     {
         public string Text;
-        public bool IsTextAnswers;
+        public bool IsTextAnswers = true;
+        public bool IsQuestionImage = true;
+        [Range(0, 3)] public int Correct;
 
-        [SerializeField] private string[] answersText;
-        [SerializeField] private Sprite[] answersImage;
-
-        [Range(0, 3)]
-        public int Correct;
-
-        public string[] AnswersText => answersText;
-        public Sprite[] AnswersImage => answersImage;
+        [SerializeField, HideInInspector] private string[] answersText;
+        [SerializeField, HideInInspector] private Sprite[] answersImage;
+        [SerializeField, HideInInspector] private Sprite questionImage;
+        
+        public Sprite QuestionImage
+        {
+            get => questionImage;
+            set => questionImage = value;
+        }
+        public string[] AnswersText
+        {
+            get => answersText;
+            set => answersText = value;
+        }
+        public Sprite[] AnswersImage
+        {
+            get => answersImage;
+            set => answersImage = value;
+        }
     }
 }
--- MathMasters/Assets/Scripts/Entities/QuestionEditor.cs
+++ MathMasters/Assets/Scripts/Entities/QuestionEditor.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,19 +11,53 @@ namespace MathMasters.Entities
         {
             Question question = (Question)target;
 
-            question.Text = EditorGUILayout.TextField("Question Text", question.Text);
-            question.IsTextAnswers = EditorGUILayout.Toggle("Is Text Answers", question.IsTextAnswers);
+            SetText(question);
+            SetToggles(question);
+            SetImage(question);
+            int answersCount = SetAnswersList(question);
 
+            SetCorrectAnswer(question, answersCount);
+
+            Save—hanges();
+        }
+        private void Save—hanges()
+        {
+            serializedObject.ApplyModifiedProperties();
+            serializedObject.Update();
+            EditorUtility.SetDirty(target);
+        }
+
+        private void SetText(Question question)
+        {
+            EditorGUILayout.LabelField("Question Text", EditorStyles.boldLabel);
+            question.Text = EditorGUILayout.TextArea(question.Text, GUILayout.Height(120));
+        }
+        private void SetToggles(Question question)
+        {
+            question.IsTextAnswers = EditorGUILayout.Toggle("Is Text Answers", question.IsTextAnswers);
+            question.IsQuestionImage = EditorGUILayout.Toggle("Is Question with image", question.IsQuestionImage);
+        }
+        private void SetImage(Question question)
+        {
+            if (question.IsQuestionImage)
+            {
+                question.QuestionImage = 
+                    (Sprite)EditorGUILayout.ObjectField("Question Image", question.QuestionImage, typeof(Sprite), false);
+            }
+        }
+
+        private int SetAnswersList(Question question)
+        {
             SerializedProperty answersProperty = serializedObject.FindProperty(question.IsTextAnswers ? "answersText" : "answersImage");
             EditorGUILayout.PropertyField(answersProperty, new GUIContent("Answers"), true);
 
-            int answersCount = answersProperty.arraySize;
+            return answersProperty.arraySize;
+        }
+
+        private void SetCorrectAnswer(Question question,int answersCount)
+        {
             question.Correct = Mathf.Clamp(question.Correct, 0, Mathf.Max(answersCount - 1, 0));
-
             question.Correct = EditorGUILayout.IntSlider("Correct Answer", question.Correct, 0, Mathf.Max(answersCount - 1, 0));
-
-            serializedObject.ApplyModifiedProperties();
-            EditorUtility.SetDirty(question);
         }
     }
 }
