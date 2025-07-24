f242de28e03b7aafcc736c46df3db730330ecba3
Pavlo Yeremeiew <113441856+PavloIeremeiew@users.noreply.github.com>
2025-07-24 14:57:33 +0300

    add LoginScreen logic

--- MathMasters/Assets/DevAssets/Scripts/UI/Tabs/LoginScreen.cs
+++ MathMasters/Assets/DevAssets/Scripts/UI/Tabs/LoginScreen.cs
@@ -1,19 +1,42 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MathMasters
 {
     public class LoginScreen : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [SerializeField]
+        private TMP_InputField _emailInput;
+
+        [SerializeField]
+        private TMP_InputField _passwordInput;
+
+        [SerializeField]
+        private Button _login;
+
+        public event Action<string, string> OnLoginAttempted;
+
+        private void OnEnable()
         {
-        
+            _login.onClick.AddListener(OnLoginButtonClicked);
+        }
+        private void OnDisable()
+        {
+            _login.onClick.RemoveListener(OnLoginButtonClicked);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnLoginButtonClicked()
         {
-        
+            string email = _emailInput.text.Trim();
+            string password = _passwordInput.text.Trim();
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                Debug.LogWarning("Email and password cannot be empty.");
+                return;
+            }
+            OnLoginAttempted?.Invoke(email, password);
         }
     }
 }
