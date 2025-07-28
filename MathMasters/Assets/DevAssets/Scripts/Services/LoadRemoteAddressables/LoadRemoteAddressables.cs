5de61a569f92ab6677aa42a68a914be1fbb36166
Pavlo Yeremeiew <113441856+PavloIeremeiew@users.noreply.github.com>
2025-07-28 21:23:02 +0300

    fix Addressables

--- MathMasters/Assets/DevAssets/Scripts/Services/LoadRemoteAddressables/LoadRemoteAddressables.cs
+++ MathMasters/Assets/DevAssets/Scripts/Services/LoadRemoteAddressables/LoadRemoteAddressables.cs
@@ -1,20 +1,19 @@
-using MathMasters.Entities;using System;
-using UnityEditor.VersionControl;
+using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace MathMasters
 {
-    public class LoadRemoteAddressables: ILoadRemoteAddressables
+    public class LoadRemoteAddressables : ILoadRemoteAddressables
     {
         public void LoadAsset<T>(string address, Action<T> onSuccess, Action onFailure = null) where T : UnityEngine.Object
         {
-            onFailure+= () => Debug.LogError($"Failed to load addressable: {address}");
+            onFailure += () => Debug.LogError($"Failed to load addressable: {address}");
             Addressables.LoadAssetAsync<T>(address).Completed += (handle) => OnLoaded(handle, onSuccess, onFailure);
         }
 
-        private void OnLoaded<T>(AsyncOperationHandle<T> handle, Action<T> onSuccess, Action onFailure) where T: UnityEngine.Object
+        private void OnLoaded<T>(AsyncOperationHandle<T> handle, Action<T> onSuccess, Action onFailure) where T : UnityEngine.Object
         {
             if (handle.Status == AsyncOperationStatus.Succeeded)
             {
